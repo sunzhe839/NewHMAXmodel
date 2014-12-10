54ece60689d4f51826fec04907baea61e25b39ec
y-miz <y-miz@users.noreply.github.com>
2014-12-11 00:46:54 +0900

    Update and rename MLocalInvariance to MLocalInvariance.py

--- MLocalInvariance.py
+++ MLocalInvariance.py
@@ -1,7 +1,7 @@
 __author__ = 'yuki'
 #coding UTF-8
 
-import numpy
+import numpy as np
 from MFilter import Filter
 
 class LocalInvariance(Filter):
@@ -10,4 +10,4 @@ class LocalInvariance(Filter):
         self.oCount = oCount #number of orientation
 
     def ComputeUnit(self, Input_p1, Input_p2):
-        return max(numpy.max(Input_p1), numpy.max(Input_p2))
+        return max(np.max(Input_p1), np.max(Input_p2))
