6b05515c2f9f28eaa8bd502556dbdd984b65a17f
y-miz <y-miz@users.noreply.github.com>
2014-12-11 00:45:58 +0900

    Update and rename MFilter to MFilter.py

--- MFilter.py
+++ MFilter.py
@@ -35,15 +35,16 @@ class Filter(object):
             space = 1.0 * Input.get_xySpace(s) / Input.get_xySpace(s + 1)
             
             for o in range(self.oCount):
+                o_x = 0
                 for x in range(0, size - self.xyCount + 1, Step):
                     x2 = x + self.xyCount
                     u_x1 = int(x * space)
                     u_x2 = int((x + self.xyCount) * space)
-                    o_x = (x + 1)/Step - 1
+                    o_y = 0
                     for y in range(0, size - self.xyCount + 1, Step):
                         y2 = y + self.xyCount
                         u_y1 = int(y * space)
                         u_y2 = int((y + self.xyCount) * space)
-                        o_y = (y + 1)/Step - 1
-                        #print u_x1, u_x2, u_y1, u_y2
                         outputimg[o_x, o_y, o] = self.ComputeUnit(i_img1[x:x2, y:y2, o], i_img2[u_x1:u_x2, u_y1:u_y2, o])
+                        o_y = o_y + 1
+                    o_x = o_x + 1
