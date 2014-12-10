6776080af56e8622f2c8aeec2041f2446da7bfb5
y-miz <y-miz@users.noreply.github.com>
2014-12-11 00:41:54 +0900

    Update Mmain.py

--- Mmain.py
+++ Mmain.py
@@ -1,26 +1,27 @@
 __author__ = 'yuki'
 #coding UTF-8
 
-import numpy
+import numpy as np
 from PIL import Image
 from PIL import ImageOps
 from MLayer import Layer
 from MGaborFilter import GaborFilter
 from MLocalInvariance import LocalInvariance
-from MPrintIMG import PrintIMG
+from MPrintIMG import Print
 
-if __name__=="__main__":    
+if __name__=="__main__":
     #test s1 to c1
-    input_size = 100 #input image size
+    input_size = 256 #input image size
     n_scale = 2 #number of scale at InputLayer
     c1_n_scale = n_scale - 1 #number of scale at c1
     n_orientation = 4
-    
+
     #input an image
     Im_name = raw_input()
     image = Image.open(Im_name)
-    
-    fs1 = GaborFilter(11, 0.4, 2.0, 1.0, n_orientation)
+
+    #fs1 = GaborFilter(11, 0.4, 2.0, 1.0, n_orientation)  :previous parameter
+    fs1 = GaborFilter(11, 0.4, 4.0, 1.5, n_orientation)
     fc1 = LocalInvariance(10, n_orientation)
 
     ImageOps.grayscale(image).resize((input_size, input_size)).save('base.jpg')
@@ -41,11 +42,8 @@ if __name__=="__main__":
     fc1.ComputeC1Layer(s1Layer, c1Layer, c1_n_scale, 5)
 
     #print orientated image
-    PrintIMG(s1Layer.get_size(0), s1Layer.get_array(0), n_orientation, "s1")
-    PrintIMG(c1Layer.get_size(0), c1Layer.get_array(0), n_orientation, "c1")
-
-
-
+    Print().PrintIMG(s1Layer.get_size(0), s1Layer.get_array(0), n_orientation, "s1")
+    Print().PrintIMG(c1Layer.get_size(0), c1Layer.get_array(0), n_orientation, "c1")
 
 
 
